d9ac69c772a47669d9d3ae247c3a95fec5fb5508
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-04 02:05:49 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,9 @@
 class CashRegister
     attr_accessor :total, :new 
     
-    def initialization(total, new = 0)
-        @total = total = 0
-        @new = new = 0
+    def initialization(total, new)
+        @total = 0
+        @new = new
     end    
     
 end    
