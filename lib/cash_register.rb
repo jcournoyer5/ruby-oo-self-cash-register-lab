11af795e8869a39655efa8c427e59026ad3dd53b
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-06 02:54:23 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
     def new=(total)
         
     def initialization(total = 0)
-        @total == 0
+        @total = 0
        
     end
        
@@ -21,28 +21,13 @@ class CashRegister
     
 end    
 
-class Person   
- attr_accessor :name   
- 
-    
- def initialize(name)
-        @name = name
-        
- end    
- 
- def self.all
-        
- end
  
  def add_item(title, price, quantity)
      
-     @total 
-     
-     @total = @total + (price * quantity)
+     @total += (price * quantity)
  end
   #   add_item(("tomato", 1.76, 2) 
 
  def void_last_transaction(title, quantity)
     
  end   
-end
