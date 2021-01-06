8d15a775dfede541a3674bbdd02344ee342759b0
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-06 02:01:52 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,4 +6,32 @@ class CashRegister
         @new = new
     end    
     
+    def apply_discount
+        
+    end    
+    
 end    
+
+class Person   
+ attr_accessor :name   
+ 
+ @@all = []    
+ def initialize(name)
+        @name = name
+        @@all << self
+ end    
+ 
+ def self.all
+        @@all
+ end
+ 
+ def add_item(title, price, quantity)
+     @total
+     
+     price_total = @total + (price * quantity)
+ end
+
+def remove_item(title, quantity)
+    
+end   
+end
